7daf0be374aebc005b9a48e9ec038ac5871b5aad
Luiz Henrique Ramos <luizgthub@gmail.com>
2025-05-11 21:53:20 -0300

    cadastro user

--- src/api/routes/users.py
+++ src/api/routes/users.py
@@ -1,8 +1,25 @@
 from fastapi import APIRouter
+from src.api.dtos.users import UserRegistration
+from src.datalayer.models.user import UserModel
 
-router = APIRouter()
+router = APIRouter(
+  prefix="/users",
+  tags=["users"],
+  responses={404: {"description": "Not Found"}}
+)
 
 
-@router.get("/")
-async def home():
-  return {"status": "ok"}
+@router.post("/register")
+async def register(body: UserRegistration):
+  user = await UserModel.create(
+    name = body.name,
+    email = body.email,
+    password = body.password
+  )
+  return {"created": user}
+
+
+@router.get("/get-users")
+async def register():
+  users = await UserModel.all()
+  return {"users": users}
